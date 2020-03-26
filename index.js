235512a26e2ca2b7fcd9d35d09feecdbc43fd0fb
TheTrueSoul <albgumifahd@gmail.com>
2020-03-26 21:19:02 +0300

    v0.1.5

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const snoowrap = require('snoowrap');
-const http = require('http');
-const port = process.env.PORT || 3000
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-  });
-  
-  server.listen(port,() => {
-    console.log(`Server running at port `+port);
-  });
 
 const r = new snoowrap({
     userAgent: 'gamedealsDiscordBot',
