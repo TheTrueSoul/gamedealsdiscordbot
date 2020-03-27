aa4b2509fc25405505cd96ef387f9b21a8b7c777
TheTrueSoul <albgumifahd@gmail.com>
2020-03-27 18:41:47 +0300

    v0.1.7

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const snoowrap = require('snoowrap');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const http = require('http');
-const express = require('express');
-const app = express();
-
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
 
 const r = new snoowrap({
     userAgent: 'gamedealsDiscordBot',
@@ -23,7 +10,7 @@ const r = new snoowrap({
 });
 
 r.config({ continueAfterRatelimitError: true });
-console.log('TESTING')
+console.log('Starting Feed..')
 var currentChannel;
 
 client.on('message', message => {
